38682668520e8162d7e2da703e768ee029f014a3
Przemek Kuczynski <przemek@pkuczynski.pl>
2016-10-22 18:14:12 +0200

    First test (home.controller.test)

---
+++ src/app/features/home/home.controller.test.js
@@ -0,0 +1,17 @@
+import home from './index';
+
+describe('Controller: Home', function() {
+   let $controller;
+
+    beforeEach(angular.mock.module(home));
+
+    beforeEach(angular.mock.inject(function(_$controller_) {
+        $controller = _$controller_;
+    }));
+
+    it('name is initialized to World', function() {
+        let ctrl = $controller('HomeController');
+        expect(ctrl.name).toBe('World');
+    });
+
+});
