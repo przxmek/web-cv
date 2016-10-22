f5fb53cd191bfb24b6a4249c8e716bd606df46bb
Przemek Kuczynski <przemek@pkuczynski.pl>
2016-10-22 18:06:26 +0200

    First directive (greeting)

--- src/app/features/home/index.js
+++ src/app/features/home/index.js
@@ -4,10 +4,11 @@ import uirouter from 'angular-ui-router';
 import routing from './home.routes';
 import HomeController from './home.controller';
 import randomNames from '../../services/randomNames.service';
+import greeting from '../../directives/greeting.directive';
 
 import './home.css';
 
-export default angular.module('app.home', [uirouter, randomNames])
+export default angular.module('app.home', [uirouter, randomNames, greeting])
     .config(routing)
     .controller('HomeController', HomeController)
     .name;
